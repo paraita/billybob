f454d7115ab0bd758291a50e0fe71eb5b46d7009
Paraita Wohler <milkyboi@gmail.com>
2016-12-19 19:16:43 +0100

    Add travis and coveralls stuff and the dependencies

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='billybob',
+      version='0.1',
+      description="BillyBob is the really handy deputy sheriff you didn't know you needed",
+      url='http://github.com/paraita/billybob',
+      author='Paraita Wohler',
+      author_email='paraita.wohler@gmail.com',
+      license='MIT',
+      packages=['billybob'],
+      install_requires=[
+          'slackclient'
+      ],
+      test_suite='nose.collector',
+      tests_require=[
+          'mock',
+          'nose',
+          'coverage',
+          'coveralls'
+      ],
+      zip_safe=False)
