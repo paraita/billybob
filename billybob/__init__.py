f4ceb3fdb76c42baa7bdd5aaf1120a4691ea454c
Paraita Wohler <milkyboi@gmail.com>
2016-12-19 20:20:59 +0100

    Create the billybob class

--- billybob/__init__.py
+++ billybob/__init__.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 
-class BillyBob():
 
-    def __init__(self):
-        pass
+class BillyBob:
+
+    def __init__(self, **kargs):
+        self._params = kargs
+
+    def print_params(self):
+        for param in self._params:
+            print '{0}={1}'.format(param, self._params[param])
--- main.py
+++ main.py
@@ -4,4 +4,6 @@
 from billybob import BillyBob
 
 
-bb = BillyBob()
+bb = BillyBob(SLACK_API_TOKEN="slack api token", JENKINS_TOKEN="jenkins token")
+
+bb.print_params()
