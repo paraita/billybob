92986382e19e4f2f0d8f1759c719c5406baac6ba
Paraita Wohler <milkyboi@gmail.com>
2016-12-20 08:16:27 +0100

    Remove the tokens

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from billybob import BillyBob
 
 
-bb = BillyBob(slack_token="xoxb-67971489922-kA41fi3F2LANkkdbsOhlOGuD",
-              wit_token="PGCJHLO7LMO656YWKFNYLIIOOPEQL7GH")
+bb = BillyBob()
 
 bb.start_service()
